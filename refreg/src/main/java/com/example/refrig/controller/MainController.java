e4e6bba3fdf8699e55db3ceebddcc26f1acaa103
Atty <eugenetkachevs@gmail.com>
2020-05-28 23:06:53 +0800

    Add dependency Author to BD

--- refreg/src/main/java/com/example/refrig/controller/MainController.java
+++ refreg/src/main/java/com/example/refrig/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.example.refrig.controller;
 
 import com.example.refrig.domain.Message;
+import com.example.refrig.domain.User;
 import com.example.refrig.repos.MessageRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,8 +34,12 @@ public class MainController {
     }
 
     @PostMapping("main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model) {
+        Message message = new Message(text, tag, user);
 
         messageRepo.save(message);
 
